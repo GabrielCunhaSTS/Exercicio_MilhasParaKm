6957b3707cd9ecc1fe554ee5a09fbe3729159427
Gabriel <ggabrielcunha06@gmail.com>
2025-03-18 18:48:36 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double milhas;
+
+Console.Write("Entre com a medida (em milhas): ");
+while (!double.TryParse(Console.ReadLine(), out milhas) || milhas < 0) 
+{
+    Console.Write("Caractere inválido! Digite um número (positivo) válido ): ");
+}
+
+milhas *= 1.609;
+
+Console.WriteLine($"O valor em quilômetros é: {milhas}");
